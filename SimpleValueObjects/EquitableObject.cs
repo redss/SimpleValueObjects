c31711187b31c72c4373e435ff4b4ed1b9b2e693
redss <ryszard.tarajkowski@gmail.com>
2017-08-20 16:11:38 +0200

    EquitableObject prototype.

---
+++ SimpleValueObjects/EquitableObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// todo: check all these warnings
+#pragma warning disable 660,661,659 // we expect child class to implement GetHashCode
+
+namespace SimpleValueObjects
+{
+    public abstract class EquitableObject<T> : IEquatable<T>
+        where T : EquitableObject<T>
+    {
+        public static bool operator !=(EquitableObject<T> first, EquitableObject<T> second)
+        {
+            return !(first == second);
+        }
+
+        public static bool operator ==(EquitableObject<T> first, EquitableObject<T> second)
+        {
+            return ReferenceEquals(first, null)
+                ? ReferenceEquals(second, null)
+                : first.Equals(second);
+        }
+
+        public override bool Equals(object other)
+        {
+            return EqualsWithNullCheck(other as T);
+        }
+
+        bool IEquatable<T>.Equals(T notNullOther)
+        {
+            return EqualsWithNullCheck(notNullOther);
+        }
+
+        private bool EqualsWithNullCheck(T other)
+        {
+            return other != null && Equals(other);
+        }
+
+        protected abstract bool Equals(T notNullOther);
+    }
+}
